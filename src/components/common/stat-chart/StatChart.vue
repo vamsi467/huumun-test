9d28d691c548ac16d27c583bff207dac7ef9c1c8
VamsiWurkr <97428498+VamsiWurkr@users.noreply.github.com>
2023-12-13 10:48:56 +0530

    Stat chart added

---
+++ src/components/common/stat-chart/StatChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="stat-chart">
+    <h2>{{ options.title }}</h2>
+    <div class="stat-chart__items">
+      <div v-for="item in options.items" :key="item.label" class="stat-chart__item">
+        <font-awesome-icon v-if="item.icon" size="5x" :icon="item.icon" :style="{ color: item.color }" />
+        <div>
+          <div v-if="item.label" class="label">{{ item.label }}</div>
+          <div v-if="item.subLabel" class="sub-label">{{ item.subLabel }}</div>
+          <div class="value" :style="{ color: item.color }">{{ item.value }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, PropType } from "vue";
+  import { IStatChart } from "./StatChart.types";
+
+  export default defineComponent({
+    name: "StatChart",
+    props: {
+      options: {
+        type: Object as PropType<IStatChart>,
+        required: true,
+      },
+    },
+  });
+</script>
+<style scoped lang="scss">
+  .stat-chart {
+    &__items {
+      display: flex;
+    }
+    &__item {
+      margin: 10px;
+
+      display: flex;
+      & > * {
+        padding: 0.75rem;
+      }
+    }
+    .label {
+      font-weight: bold;
+      margin-bottom: 0.5rem;
+    }
+    .value {
+      font-size: 2.5rem;
+      font-weight: bold;
+      margin-bottom: 0.5rem;
+    }
+    .sub-label {
+      color: #7e7e7e;
+    }
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,20 @@ import router from "@/router";
 
 import "@/assets/scss/style.scss";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faPerson, faPersonDress } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faPerson, faPersonDress);
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.mount("#app");
